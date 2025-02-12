373c847c565c66aca2540977358b000b64c60ae7
TobiTRy <tobiastry@aol.de>
2025-02-12 21:17:55 +0100

    feat: Update size settings and component props in FancySystemMessageBox for improved layout and consistency

--- src/components/templates/FancySystemMessageBox/sizeSettings.ts
+++ src/components/templates/FancySystemMessageBox/sizeSettings.ts
@@ -5,34 +5,18 @@ import { TSpacings } from '@/types/TSpacings';
 import { TComponentSizes } from '@/types';
 
 type TSizeObj = {
-  iconSize: TComponentSizes;
-  title: TTypographyVariants;
-  descriptionSize: TTypographyVariants;
-  systemMessageSize: TSystemMessageBox['sizeC'];
-  gap: TSpacings;
+  systemMessageSize: TComponentSizes;
 };
 
 // the template for the sizes
 export const sizes: TSizeSettings<TSizeObj> = {
   sm: {
-    iconSize: 'sm',
-    title: 'subTextCaption',
-    descriptionSize: 'subTextFootnote',
     systemMessageSize: 'sm',
-    gap: 'xs',
   },
   md: {
-    iconSize: 'sm',
-    title: 'bodytextSm',
-    descriptionSize: 'subTextCaption',
     systemMessageSize: 'md',
-    gap: 'xs',
   },
   lg: {
-    iconSize: 'md',
-    title: 'bodytextMd',
-    descriptionSize: 'bodytextSm',
     systemMessageSize: 'lg',
-    gap: 'xs',
   },
 };
